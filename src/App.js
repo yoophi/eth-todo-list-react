7b076d8a47850e74693cf11fed7e1a1c716f2f58
Pyunghyuk Yoo <yoophi@gmail.com>
2021-05-29 21:55:07 +0900

    ✨ NEW: setup TodoList Contract and get taskCount

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import "./App.css";
 
-import { useCallback, useEffect, useState } from "react";
+import { TODO_LIST_ABI, TODO_LIST_ADDRESS } from "./config";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
 import Web3 from "web3";
 
 function App() {
   const [account, setAccount] = useState("");
+  const [todoList, setTodoList] = useState(null);
+  const [taskCount, setTaskCount] = useState(0);
+  const web3 = useMemo(() => {
+    return new Web3("http://localhost:7545");
+  });
   const loadBlockchainData = useCallback(async () => {
-    const web3 = new Web3("http://localhost:7545");
     const accounts = await web3.eth.getAccounts();
     setAccount(accounts[0]);
-  }, [setAccount]);
+  }, [web3, setAccount]);
+
+  useEffect(() => {
+    const init = async () => {
+      const todoList = new web3.eth.Contract(TODO_LIST_ABI, TODO_LIST_ADDRESS);
+      const taskCount = await todoList.methods.taskCount().call();
+      setTodoList(todoList);
+      setTaskCount(taskCount);
+    };
+    init();
+  }, [web3]);
 
   useEffect(() => {
     loadBlockchainData();
@@ -18,7 +33,8 @@ function App() {
   return (
     <div className="container">
       <h1>Hello, World!</h1>
-      {account && <p>Your account: {account}</p>}
+      <div>{account && <p>Your account: {account}</p>}</div>
+      <div>taskCount: {taskCount}</div>
     </div>
   );
 }
