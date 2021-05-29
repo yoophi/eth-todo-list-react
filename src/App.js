158ea7f4b2b930b4e581cc68c7fd5cf9dc91749b
Pyunghyuk Yoo <yoophi@gmail.com>
2021-05-29 23:06:42 +0900

    ✨ NEW: load Tasks

--- src/App.js
+++ src/App.js
@@ -7,34 +7,50 @@ import Web3 from "web3";
 
 function App() {
   const [account, setAccount] = useState("");
-  const [todoList, setTodoList] = useState(null);
+  const [tasks, setTasks] = useState([]);
   const [taskCount, setTaskCount] = useState(0);
   const web3 = useMemo(() => {
     return new Web3("http://localhost:7545");
-  });
-  const loadBlockchainData = useCallback(async () => {
+  }, []);
+
+  const todoList = useMemo(() => {
+    return new web3.eth.Contract(TODO_LIST_ABI, TODO_LIST_ADDRESS);
+  }, [web3]);
+
+  const loadAccount = useCallback(async () => {
     const accounts = await web3.eth.getAccounts();
     setAccount(accounts[0]);
   }, [web3, setAccount]);
 
-  useEffect(() => {
-    const init = async () => {
-      const todoList = new web3.eth.Contract(TODO_LIST_ABI, TODO_LIST_ADDRESS);
-      const taskCount = await todoList.methods.taskCount().call();
-      setTodoList(todoList);
-      setTaskCount(taskCount);
-    };
-    init();
-  }, [web3]);
+  const loadTaskData = useCallback(async () => {
+    const taskCount = Number(await todoList.methods.taskCount().call());
+    console.log(`taskCount = ${taskCount}`);
+    const tasks = await Promise.all(
+      [...Array(taskCount).keys()].map(async (id) => {
+        console.log(`loading task(${id + 1})`);
+        const task = await todoList.methods.tasks(id + 1).call();
+        return {
+          id: task.id,
+          countent: task.content,
+          completed: task.completed,
+        };
+      })
+    );
+    setTasks(tasks);
+    setTaskCount(taskCount);
+  }, [todoList, setTasks]);
 
   useEffect(() => {
-    loadBlockchainData();
-  }, [loadBlockchainData]);
+    loadAccount();
+    loadTaskData();
+  }, [loadAccount, loadTaskData]);
+
   return (
     <div className="container">
       <h1>Hello, World!</h1>
       <div>{account && <p>Your account: {account}</p>}</div>
       <div>taskCount: {taskCount}</div>
+      <pre>{JSON.stringify(tasks, null, 2)}</pre>
     </div>
   );
 }
